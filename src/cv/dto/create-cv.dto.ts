1fc35e1471312ddda312ae58ed7dfe7ac68ca044
BenArbiaAhmed <hamoudabenarbia15@gmail.com>
2026-04-10 21:23:08 +0100

    add cv dtos

--- src/cv/dto/create-cv.dto.ts
+++ src/cv/dto/create-cv.dto.ts
@@ -1,1 +1,39 @@
-export class CreateCvDto {}
+import {
+  IsString,
+  IsNumber,
+  IsArray,
+  IsNotEmpty,
+  IsOptional,
+  Min,
+} from 'class-validator';
+
+export class CreateCvDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  age: number;
+
+  @IsString()
+  @IsNotEmpty()
+  cin: string;
+
+  @IsString()
+  @IsNotEmpty()
+  job: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  userId: number;
+
+  @IsArray()
+  @IsOptional()
+  skillIds?: number[];
+}
